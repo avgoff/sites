a17945d07eeb366a53a965a61638955bfb575bed
Alexander <avgoff1@gmail.com>
2025-02-20 15:48:15 +0300

    final

--- Pulse_step/src/js/script.js
+++ Pulse_step/src/js/script.js
@@ -31,6 +31,88 @@ $(document).ready(function(){
 
   toggleClass('.catalog-item__link')
   toggleClass('.catalog-item__back')
+
+  //modal
+  $('[data-modal="consultation"]').on('click', function(  ){
+    $('.overlay, #consultation').fadeIn();
+  });
+  $('.modal__close').on('click', function(){
+    $('.overlay, #consultation, #order, #thx').fadeOut();
+  });
   
+
+  $('.button_mini').each(function(i){
+    $(this).on('click', function(){
+      $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+      $('.overlay, #order').fadeIn();
+    })
+  })
+
+  $('#consultation-form').validate();
+  $('#consultation-form').validate( {
+    rules:{
+      name:"required",
+      phone:"required",
+      email:{
+        required: true,
+        email: true
+      }
+    }
+  });
+  $('#order form').validate();
+
+  $('input[name=phone]').mask("+375 (99) 999-99-99");
+
+  $('form').submit(function(e){
+    e.preventDefault();
+    if(!$(this).valid()){
+      return;
+    }
+
+    $.ajax({
+      type:"POST",
+      url: "mailer/smart.php",
+      data: $(this).serilize()
+    }).done(function(){
+      $(this).find("input").val("");
+      $('#consultation, #order').fadeOut();
+      $('.overlay, #thx').fadeIn();
+      $('form').trigger('reset');
+    });
+    return false;
+  });
+
+  //pageUp
+  $(window).scroll(function(){
+    if($(this).scrollTop()>1600){
+      $('.pageup').fadeIn()
+    } else{
+      $('.pageup').fadeOut()
+    }
+  })
+
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+
+  new WOW().init();
 });
 		
